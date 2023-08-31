6bb30c4d7fa32adfcc48a66c00975bf19e673069
Leonardo Flores <contatoleonardoflores@gmail.com>
2023-08-31 16:00:41 -0300

    feat: added jwt into the cookie

--- pages/api/auth/signin.ts
+++ pages/api/auth/signin.ts
@@ -3,6 +3,7 @@ import validator from 'validator';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import * as jose from 'jose';
+import { setCookie } from 'cookies-next';
 
 interface BodyResponseProps {
   email: string;
@@ -17,11 +18,11 @@ interface ValidationSchemaProps {
   errorMessage: string;
 }
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    const { email, password } = req.body as BodyResponseProps
+    const { email, password } = req.body as BodyResponseProps;
 
     const validationSchema: ValidationSchemaProps[] = [
       {
@@ -51,38 +52,47 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       });
     }
 
-    const userWithEmail = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
         email,
-      }
-    })
+      },
+    });
 
-    if (!userWithEmail) {
+    if (!user) {
       return res.status(401).json({
-        errorMessage: 'Email or password is invalid'
-      })
+        errorMessage: 'Email or password is invalid',
+      });
     }
 
-    const isMatch = await bcrypt.compare(password, userWithEmail.password)
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return res.status(401).json({
-        errorMessage: 'Email or password is invalid'
-      })
+        errorMessage: 'Email or password is invalid',
+      });
     }
 
     const alg = 'HS256';
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-    const token = await new jose.SignJWT({ email: userWithEmail.email })
+    const token = await new jose.SignJWT({ email: user.email })
       .setProtectedHeader({ alg })
       .setExpirationTime('24h')
       .sign(secret);
 
+    setCookie('jwt', token, {
+      req,
+      res,
+      maxAge: 60 * 6 * 24,
+    });
+
     return res.status(200).json({
-      message: 'Login successful',
-      token
-    })
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+      city: user.city,
+      phone: user.phone,
+    });
   }
 
   res.status(501).send({
