421e7db14b86d5229c12a4de3a19503da99ba135
Leonardo Flores <contatoleonardoflores@gmail.com>
2023-09-04 12:08:45 -0300

    feat: added request method validation

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -11,45 +11,51 @@ interface JWTPayloadProps {
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const bearerToken = req.headers.authorization as string;
-  const token = bearerToken.split(' ')[1];
+  if (req.method === 'GET') {
+    const bearerToken = req.headers.authorization as string;
+    const token = bearerToken.split(' ')[1];
 
-  const errorMessage = 'Unauthorized, access is denied due to invalid credentials';
+    const errorMessage = 'Unauthorized, access is denied due to invalid credentials';
 
-  const payload = jwt.decode(token) as JWTPayloadProps;
+    const payload = jwt.decode(token) as JWTPayloadProps;
 
-  if (!payload.email) {
-    return res.status(401).json({
-      errorMessage,
+    if (!payload.email) {
+      return res.status(401).json({
+        errorMessage,
+      });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+      select: {
+        id: true,
+        first_name: true,
+        last_name: true,
+        email: true,
+        city: true,
+        phone: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        errorMessage: 'User not found',
+      });
+    }
+
+    return res.status(200).json({
+      id: user.id,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+      city: user.city,
+      phone: user.phone,
     });
   }
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: payload.email,
-    },
-    select: {
-      id: true,
-      first_name: true,
-      last_name: true,
-      email: true,
-      city: true,
-      phone: true,
-    }
-  });
-
-  if (!user) {
-    return res.status(401).json({
-      errorMessage: 'User not found'
-    })
-  }
-
-  return res.status(200).json({
-    id: user.id,
-    firstName: user.first_name,
-    lastName: user.last_name,
-    email: user.email,
-    city: user.city,
-    phone: user.phone,
+  res.status(501).send({
+    error: 'Sorry, the request method is not supported by the server and cannot be handled.',
   });
 }
--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,77 +1,84 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 
-import { findAvailableTables } from '../../../../services/restaurant/findAvailableTables'
+import { findAvailableTables } from '../../../../services/restaurant/findAvailableTables';
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { slug, day, time, partySize } = req.query as {
-    slug: string;
-    day: string;
-    time: string;
-    partySize: string;
-  };
-
-  const errorMessage = 'Invalid data provided';
-
-  if (!day || !time || !partySize) {
-    return res.status(400).json({
-      errorMessage,
-    });
-  }
-
-  const restaurant = await prisma.restaurant.findUnique({
-    where: {
-      slug,
-    },
-    select: {
-      tables: true,
-      open_time: true,
-      close_time: true,
-    },
-  });
-
-  if (!restaurant) {
-    return res.status(400).json({
-      errorMessage,
-    });
-  }
-
-  const searchTimesWithTables = await findAvailableTables({
-    res,
-    time,
-    day,
-    errorMessage,
-    restaurant,
-  })
-
-  if (!searchTimesWithTables) {
-    return res.status(400).json({
-      errorMessage,
-    });
-  }
-
-  const availabilities = searchTimesWithTables.map((searchTimesWithTable) => {
-    const sumSeats = searchTimesWithTable.tables.reduce((acc, table) => acc + table.seats, 0);
-
-    const parsedPartySize = parseInt(partySize, 10);
-
-    return {
-      time: searchTimesWithTable.time,
-      available: sumSeats >= parsedPartySize,
+  if (req.method === 'GET') {
+    const { slug, day, time, partySize } = req.query as {
+      slug: string;
+      day: string;
+      time: string;
+      partySize: string;
     };
-  })
-  .filter(availability => {
-    const availabilityDateTime = new Date(`${day}T${availability.time}`)
-    const openDateTime = new Date(`${day}T${restaurant.open_time}`)
-    const closeDateTime = new Date(`${day}T${restaurant.close_time}`)
 
-    const timeIsAfterOpeningHour = availabilityDateTime >= openDateTime
-    const timeIsBeforeClosingHour = availabilityDateTime <= closeDateTime
+    const errorMessage = 'Invalid data provided';
 
-    return timeIsAfterOpeningHour && timeIsBeforeClosingHour
+    if (!day || !time || !partySize) {
+      return res.status(400).json({
+        errorMessage,
+      });
+    }
+
+    const restaurant = await prisma.restaurant.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        tables: true,
+        open_time: true,
+        close_time: true,
+      },
+    });
+
+    if (!restaurant) {
+      return res.status(400).json({
+        errorMessage,
+      });
+    }
+
+    const searchTimesWithTables = await findAvailableTables({
+      res,
+      time,
+      day,
+      errorMessage,
+      restaurant,
+    });
+
+    if (!searchTimesWithTables) {
+      return res.status(400).json({
+        errorMessage,
+      });
+    }
+
+    const availabilities = searchTimesWithTables
+      .map((searchTimesWithTable) => {
+        const sumSeats = searchTimesWithTable.tables.reduce((acc, table) => acc + table.seats, 0);
+
+        const parsedPartySize = parseInt(partySize, 10);
+
+        return {
+          time: searchTimesWithTable.time,
+          available: sumSeats >= parsedPartySize,
+        };
+      })
+      .filter((availability) => {
+        const availabilityDateTime = new Date(`${day}T${availability.time}`);
+        const openDateTime = new Date(`${day}T${restaurant.open_time}`);
+        const closeDateTime = new Date(`${day}T${restaurant.close_time}`);
+
+        const timeIsAfterOpeningHour = availabilityDateTime >= openDateTime;
+        const timeIsBeforeClosingHour = availabilityDateTime <= closeDateTime;
+
+        return timeIsAfterOpeningHour && timeIsBeforeClosingHour;
+      });
+
+    return res.status(200).json(availabilities);
+  }
+
+  res.status(501).send({
+    error: 'Sorry, the request method is not supported by the server and cannot be handled.',
   });
-
-  return res.status(200).json(availabilities);
 }
